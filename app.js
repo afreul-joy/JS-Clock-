1e69722034eb9ce1ccaa136c70f3bf279a9ad6aa
AFreul Joy <afreulxoy@gmail.com>
2021-06-09 15:23:27 +0600

    First commit

---
+++ app.js
@@ -0,0 +1,43 @@
+function digitalClock() {
+  let date = new Date();
+  let hours = date.getHours(); //0-23
+  let minutes = date.getMinutes(); 
+  let seconds = date.getSeconds(); 
+  let timeFormat = 'AM'
+
+   
+if (hours === 12) {       //12 tar besi hole seta PM hbe
+  timeFormat="PM"
+}
+else if (hours === 0) {
+  hours = 12;
+}
+else {
+  hours;
+  timeFormat = "AM"
+  }
+
+if (hours>12) {
+  hours = hours - 12
+  timeFormat = "PM"
+  }
+  else {
+  hours;
+  timeFormat = "AM"
+}
+
+  hours < 10 ? hours = '0' + hours : hours
+  minutes < 10 ? minutes = '0' + minutes : minutes;
+  seconds < 10 ? seconds = '0' + seconds : seconds;
+
+let finalTime = `${hours}:${minutes}:${seconds}`
+document.getElementById('time').innerHTML = finalTime
+document.querySelector('small').innerHTML=timeFormat 
+setInterval(digitalClock, 1000)    
+
+  }
+
+  
+digitalClock()
+
+
